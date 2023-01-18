f08d262618aa22cb599d27992291e916f0d9e6bd
pandasoli <sadpandasoli@gmail.com>
2023-01-17 23:32:35 -0300

    got back one dir

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -8,7 +8,7 @@ use types::*;
 
 
 fn main() {
-  let img = read("examples/Free Hub.png");
+  let img = read("../examples/Free Hub.png");
 
   if let Ok(img) = img {
     let new_dims = Rect::new(328, 340, 100, 100);
@@ -18,7 +18,7 @@ fn main() {
       let img = image::load_from_memory(res.as_slice());
 
       if let Ok(img) = img {
-        let res = img.save("results/Free Hub.png");
+        let res = img.save("../results/Free Hub.png");
         println!("{:?}", res);
       }
     }
