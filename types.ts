a38641c6aa4c96d94509859ee4974bdeb072165d
pandasoli <sadpandasoli@gmail.com>
2023-01-18 01:56:43 -0300

    added: res

--- types.ts
+++ types.ts
@@ -1,9 +1,13 @@
 
-export type ICube = {
+export type Rect = {
   x: number
   y: number
-
   w: number
   h: number
 }
 
+export type Res = {
+  status: boolean
+  err: string
+  res: Uint8Array
+}
