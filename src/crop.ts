78229bae2ffe0b656dec6eaa47cbe41437a540f7
pandasoli <sadpandasoli@gmail.com>
2023-02-16 15:09:47 -0300

    style: add destructuration

--- src/crop.ts
+++ src/crop.ts
@@ -2,9 +2,9 @@ import init, { crop, Rect } from './pkg/rs.js'
 import './pkg/rs.d.ts'
 
 
-export default async (img: Uint8Array, rect: Rect): Promise<Uint8Array> => {
+export default async (img: Uint8Array, { x, y, w, h }: Rect): Promise<Uint8Array> => {
   await init()
-  const dim = new Rect(rect.x, rect.y, rect.w, rect.h)
+  const dim = new Rect(x, y, w, h)
   const res = crop(img, dim)
 
   if (res.status)
